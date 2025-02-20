08ebba3cdb10b6cac86989874f6d955d990dbdca
jeff283 <njorogejeff2019@gmail.com>
2025-02-20 20:19:40 +0300

    Added db context and initializer to program cs

--- SGames25Api/Program.cs
+++ SGames25Api/Program.cs
@@ -1,8 +1,28 @@
+using SGames25Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(o =>
+    {
+        o.JsonSerializerOptions.DefaultIgnoreCondition
+            = JsonIgnoreCondition.WhenWritingDefault;
+    });
+
+//To give access to IHttpContextAccessor for Audit Data with IAuditable
+builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddDbContext<SummerGamesContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("SummerGamesConnection")));
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,5 +41,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//To prepare the database and seed data. 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SummerGamesInitializer.Initialize(serviceProvider: services, DeleteDatabase: true,
+        UseMigrations: false, SeedRandomSampleData: false);
+}
 
 app.Run();
